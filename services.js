65479320838c2e331132b139b4bd5d801ca91191
Jared Bilton <cantops@jareds-mbp.attlocal.net>
2015-11-23 23:10:42 -0500

    reinstalled ng

--- services.js
+++ services.js
@@ -1,25 +1,23 @@
 (function() {
   'use strict';
 
-    angular
-    .module('flix') //attach main module
-    .factory('FilmHub', function ($http){
-      var url = 'http://tiny-tiny.herokuapp.com/collections/rudy';
-
-// adding the movie method
-      var addFlix = function (newFlix) {
-        $http.post(url, newFlix).then(function (camp){
-          console.log(camp);
-        });
-      var getFlix = function (){
-          return $http.get(url); //going to id in controller
+  angular.module('flix') //attach main module
+    .factory('FilmHub', function($http) {
+        var url = 'http://tiny-tiny.herokuapp.com/collections/rudy';
+        // adding the movie method
+        var addFlix = function(newFlix) {
+            $http.post(url, newFlix)
         };
-        return {
-          createFlix: addFlix,
-          getFlix: getFlix //, deleteFlix: removeFlix
+    });
+        var getFlix = function() {
+            return $http.get(url); //going to id in controller
+        };
+        var removeFlix = function() {
+            $http.delete(url, deleteFlix)
         }
-      }
-    })
-
-
-}());
+        return {
+            createFlix: addFlix,
+            getFlix: getFlix,
+            deleteFlix: removeFlix
+        };
+});
