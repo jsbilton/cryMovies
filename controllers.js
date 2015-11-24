4b5ce06d2411989e628bffab36c75d15338a750c
Jared Bilton <cantops@jareds-mbp.attlocal.net>
2015-11-23 22:23:39 -0500

    added controllers

--- controllers.js
+++ controllers.js
@@ -4,6 +4,21 @@
   angular
     .module('flix')
     .controller('MainController', function ($scope){
-      $scope.alertMe = function () {}
+      $scope.alertMe = function () {
+        _.times(1000, function(){
+          console.log("hello");
+        })
+      };
+      $scope.text = "Aloha amigo";
     })
+    .controller('FlixController, function($scope, FlixHub){
+      FlixHub.getFlix().success(function (flix){
+        console.log(flix);
+        $scope.cryFilms = flix; //can use in html
+      });
+      $scope.addFlix = function (newFlix) {
+        console.log(newFlix);
+        FlixHub.createFlix(newFlix);
+      }
+    }')
 }());
