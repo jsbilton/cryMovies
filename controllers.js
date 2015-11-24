b1ee58085138f97f57f2c970cba3ec652a631b49
Jared Bilton <cantops@jareds-mbp.attlocal.net>
2015-11-23 23:50:14 -0500

    updates still errors

--- controllers.js
+++ controllers.js
@@ -1,24 +1,25 @@
 (function() {
   'use strict';
-
-  angular
-    .module('flix')
-    .controller('MainController', function ($scope){
-      $scope.alertMe = function () {
-        _.times(1000, function(){
-          console.log("hello");
-        })
-      };
-      $scope.text = "Aloha amigo";
-    })
+   angular
+   .module('flix')
+   .controller('MainController', function($scope) {
+      var updateFlix = function() {
+        FlixHub.getFlix().success(function(flix) {
+        $scope.flix = flix;
+      });
+  };
+      updateFlix();
+  });
+});
     .controller('FlixController', function($scope, FlixHub){
       FlixHub.getFlix().success(function (flix){
         console.log(flix);
         $scope.cryFilms = flix; //can use in html
       });
-      $scope.addFlix = function (newFlix) {
+    $scope.addFlix = function(newFlix) {
         console.log(newFlix);
         FlixHub.createFlix(newFlix);
-      }
-    })
-}());
+    };
+    $scope.deleteFlix = function(flix) {
+        FlixHub.deleteFlix(flix);
+    };
--- services.js
+++ services.js
@@ -6,14 +6,14 @@
         var url = 'http://tiny-tiny.herokuapp.com/collections/rudy';
         // adding the movie method
         var addFlix = function(newFlix) {
-            $http.post(url, newFlix)
+            $http.post(url, newFlix);
         };
     });
         var getFlix = function() {
             return $http.get(url); //going to id in controller
         };
         var removeFlix = function() {
-            $http.delete(url, deleteFlix)
+            $http.delete(url, deleteFlix);
         }
         return {
             createFlix: addFlix,
